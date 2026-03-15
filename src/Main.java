eac0f096ba643141ef67081ca67be11d078c296a
Sifti Al Mahmud <siftiam@gmail.com>
2026-03-15 10:58:29 +0600

    First Commit

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,21 @@
 
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.Scanner;
+
+class Employee{
+    String id;
+    String name;
+    String dob;
+    String mail;
+    LocalDate joininDate;
+    String type;
+
+    int vacationalLeave;
+    int sickLeave;
+
+}
+
 public class Main {
     public static void main(String[] args) {
 
