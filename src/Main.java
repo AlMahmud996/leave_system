71bf5a0582f399216090dc0675c961fbe75c598f
Sifti Al Mahmud <siftiam@gmail.com>
2026-03-15 11:06:50 +0600

    Creating Employee & calculateLeave

--- src/Main.java
+++ src/Main.java
@@ -14,6 +14,32 @@ class Employee{
     int vacationalLeave;
     int sickLeave;
 
+    public Employee(String id, String name, String dob, String mail, String type, LocalDate joininDate){
+        this.id = id;
+        this.name = name;
+        this.dob = dob;
+        this.mail = mail;
+        this.type = type;
+        this.joininDate = joininDate;
+
+        calculateLeave();
+    }
+
+    void calculateLeave(){
+        int vacationTotal = 0;
+        int sickTotal = 0;
+
+        if(type.equalsIgnoreCase("Officer")){
+            vacationTotal = 15;
+            sickTotal = 10;
+        }else{
+            vacationTotal = 10;
+            sickTotal = 7;
+        }
+
+
+    }
+
 }
 
 public class Main {
